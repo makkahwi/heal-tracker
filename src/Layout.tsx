8083412d7edf499be8bd69c35ba53fb1266368c4
makkahwi <suhaibahmadai@hotmail.com>
2024-06-23 21:40:13 +0300

    Build Initial 'Scheduled Meals' View

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.css";
 const Layout = ({ children = <></> }) => {
   return (
     <main>
-      <body className="bg-dark min-vh-100 text-white text-center my-auto">
+      <body className="bg-dark min-vh-100 text-white text-center p-5">
         {children}
       </body>
     </main>
