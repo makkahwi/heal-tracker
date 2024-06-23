aac46d0e3b07539ebf43149ee82d79bac92fa4eb
makkahwi <suhaibahmadai@hotmail.com>
2024-06-23 22:44:56 +0300

    Relocate Layout Comp Engagement Location

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 import { Fragment } from "react/jsx-runtime";
-import Layout from "./Layout";
 import Consumption from "./Views/Consumption";
 import Schedule from "./Views/Schedule";
 import logo from "./logo.svg";
 
 const App = () => {
   return (
-    <Layout>
-      <Fragment>
-        <img src={logo} className="App-logo" alt="logo" />
+    <Fragment>
+      <img src={logo} className="App-logo" alt="logo" />
 
-        <p>Welcome To `Personal Diet Tracker`</p>
+      <p>Welcome To `Personal Diet Tracker`</p>
 
-        <Schedule />
+      <Consumption />
 
-        <Consumption />
-      </Fragment>
-    </Layout>
+      <Schedule />
+    </Fragment>
   );
 };
 
