c4fabb7eb93d5699eb740bf4fff20c1eefcd5de4
makkahwi <suhaibahmadai@hotmail.com>
2024-06-23 23:19:14 +0300

    Engage API Simulators

--- src/Views/Schedule/index.tsx
+++ src/Views/Schedule/index.tsx
@@ -1,53 +1,16 @@
-import MealView from "../../Components/MealView";
+import { useEffect, useState } from "react";
+import * as scheduleAPI from "../../API/schedule";
+import MealView, { MealViewProps } from "../../Components/MealView";
 import PageSection from "../../Components/PageSection";
 
 const Schedule = () => {
+  const [data, setData] = useState<MealViewProps[][]>([]);
   const meals = ["Breakfast", "Light Meal", "Drink", "Lunch", "Light Meal"];
-  const schedule = [
-    [
-      { element: "Bread", count: "1 Slice" },
-      { element: "Various Vegetables", count: "Slices" },
-      { element: "Olive", count: "4 Pieces" },
-      { element: "Yoghurt", count: "1 Cup" },
-      {
-        element: "Egg",
-        count: "2",
-        alternatives: [
-          { element: "Labaneh", count: "90gm" },
-          { element: "Areesh Cheese", count: "90gm" },
-          { element: "Chickpeas", count: "100gm" },
-          { element: "Lentil", count: "100gm" },
-        ],
-      },
-    ],
-    [
-      {
-        element: "Salad",
-        count: "1",
-        alternatives: [{ element: "Various Vegetables", count: "Slices" }],
-      },
-      { element: "Yoghurt", count: "1 Cup" },
-      { element: "Fruit", count: "1 Small Piece" },
-      { element: "Walnuts", count: "4 Pieces" },
-    ],
-    [{ element: "Green Tea", count: "1 Cup" }],
-    [
-      {
-        element: "Chicken Breast",
-        count: "120gm",
-        alternatives: [
-          { element: "Fish", count: "120gm" },
-          { element: "Beef", count: "120gm" },
-        ],
-      },
-      {
-        element: "Salad",
-        count: "1",
-      },
-      { element: "Rice", count: "300gm" },
-    ],
-    [{ element: "Yoghurt", count: "1 Cup" }],
-  ];
+
+  useEffect(() => {
+    // scheduleAPI.getAll().then((res: MealViewProps[][]) => setData(res));
+    setData(scheduleAPI.getAll());
+  }, []);
 
   return (
     <PageSection title="Scheduled Meals">
@@ -61,13 +24,13 @@ const Schedule = () => {
         </thead>
 
         <tbody>
-          {schedule.map((mealContents, x) => (
+          {data.map((mealContents, x) => (
             <tr key={x}>
               <th>{meals[x % meals.length]}</th>
 
               <td>
                 <ul className="text-start">
-                  {mealContents.map(({ element, count, alternatives }, y) => (
+                  {mealContents?.map(({ element, count, alternatives }, y) => (
                     <MealView
                       count={count}
                       element={element}
