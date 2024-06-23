73873afb2700c7b9fa671bc9efd237e9b9a5e02d
makkahwi <suhaibahmadai@hotmail.com>
2024-06-23 22:45:36 +0300

    Remove React Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { Fragment } from "react/jsx-runtime";
 import Consumption from "./Views/Consumption";
 import Schedule from "./Views/Schedule";
-import logo from "./logo.svg";
 
 const App = () => {
   return (
     <Fragment>
-      <img src={logo} className="App-logo" alt="logo" />
-
       <p>Welcome To `Personal Diet Tracker`</p>
 
       <Consumption />
