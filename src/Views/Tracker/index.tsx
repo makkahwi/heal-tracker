6774d7523c7fff03af716f2b9881b9d6a60f838e
makkahwi <suhaibahmadai@hotmail.com>
2024-06-23 21:19:49 +0300

    Build Initial Views Layout

---
+++ src/Views/Tracker/index.tsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react/jsx-runtime";
+
+const Tracker = () => {
+  return (
+    <Fragment>
+      <p>This is Tracker View</p>
+    </Fragment>
+  );
+};
+
+export default Tracker;
