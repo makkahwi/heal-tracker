d04fd23e808f7bab51cdab474cc76d8cef980cc7
makkahwi <suhaibahmadai@hotmail.com>
2024-06-23 22:23:45 +0300

    Add Comparison To  'Consumed Meals' View

--- src/Views/Consumption/index.tsx
+++ src/Views/Consumption/index.tsx
@@ -15,6 +15,22 @@ const Consumption = () => {
           count: "2",
         },
       ],
+      supposed: [
+        { element: "Bread", count: "1 Slice" },
+        { element: "Various Vegetables", count: "Slices" },
+        { element: "Olive", count: "4 Pieces" },
+        { element: "Yoghurt", count: "1 Cup" },
+        {
+          element: "Egg",
+          count: "2",
+          alternatives: [
+            { element: "Labaneh", count: "90gm" },
+            { element: "Areesh Cheese", count: "90gm" },
+            { element: "Chickpeas", count: "100gm" },
+            { element: "Lentil", count: "100gm" },
+          ],
+        },
+      ],
     },
     {
       timestamp: new Date(),
@@ -23,8 +39,27 @@ const Consumption = () => {
         { element: "Various Vegetables", count: "Slices" },
         { element: "Olive", count: "4 Pieces" },
       ],
+      supposed: [
+        {
+          element: "Chicken Breast 'Boiled OR Baked OR BBQ'",
+          count: "120gm",
+          alternatives: [
+            { element: "Fish 'Boiled OR Baked OR BBQ'", count: "120gm" },
+            { element: "Beef 'Boiled OR Baked OR BBQ'", count: "120gm" },
+          ],
+        },
+        {
+          element: "Salad",
+          count: "1",
+        },
+        { element: "Rice", count: "300gm" },
+      ],
     },
   ];
+
+  const MealView = ({ count = "", element = "" }) => (
+    <li>{count + " of " + element}</li>
+  );
 
   return (
     <Fragment>
@@ -40,11 +75,17 @@ const Consumption = () => {
             <th>Meal of Day</th>
 
             <th>Consumed Meal Contents</th>
+
+            <th>Supposed To Consume Meal Contents</th>
+
+            <th>Missed Supposes</th>
+
+            <th>Added Consumptions</th>
           </tr>
         </thead>
 
         <tbody>
-          {consumption.map(({ timestamp, meal, contents }, x) => (
+          {consumption.map(({ timestamp, meal, contents, supposed }, x) => (
             <tr key={x}>
               <td>{timestamp.getDate()}</td>
               <td>{timestamp.getTime()}</td>
@@ -53,8 +94,51 @@ const Consumption = () => {
               <td>
                 <ul className="text-start">
                   {contents.map(({ element, count }, y) => (
-                    <li key={y}>{count + " of " + element}</li>
+                    <MealView count={count} element={element} />
                   ))}
+                </ul>
+              </td>
+
+              <td>
+                <ul className="text-start">
+                  {supposed.map(({ element, count }, y) => (
+                    <MealView count={count} element={element} />
+                  ))}
+                </ul>
+              </td>
+
+              <td>
+                <ul className="text-start">
+                  {contents
+                    .filter(
+                      ({ element, count }, y) =>
+                        element !==
+                          supposed.find((sup) => sup.element === element)
+                            ?.element ||
+                        count !==
+                          supposed.find((sup) => sup.element === element)?.count
+                    )
+                    .map(({ element, count }, y) => (
+                      <MealView count={count} element={element} />
+                    ))}
+                </ul>
+              </td>
+
+              <td>
+                <ul className="text-start">
+                  {supposed
+                    .filter(
+                      ({ element, count }, y) =>
+                        element !==
+                          contents.find((cont) => cont.element === element)
+                            ?.element ||
+                        count !==
+                          contents.find((cont) => cont.element === element)
+                            ?.count
+                    )
+                    .map(({ element, count }, y) => (
+                      <MealView count={count} element={element} />
+                    ))}
                 </ul>
               </td>
             </tr>
