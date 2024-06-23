2da79170a86a939eb1005e3a7c2e4db8db0fd601
makkahwi <suhaibahmadai@hotmail.com>
2024-06-24 00:31:26 +0300

    Build & Engage Form Comp

---
+++ src/Components/Form/index.tsx
@@ -0,0 +1,163 @@
+import { Fragment, useState } from "react";
+
+interface dynamicObject {
+  [key: string]: any;
+  // | number | object | string[] | number[] | object[];
+}
+
+interface inputProps {
+  name: string;
+  label: string;
+  required?: boolean;
+  type?: string;
+  options?: string[];
+  inputs?: inputProps[];
+}
+
+interface props {
+  inputs: inputProps[];
+  onSubmit: ({}) => void;
+}
+
+const Form = ({ inputs, onSubmit }: props) => {
+  const [formValues, setFormValues] = useState<dynamicObject>({});
+
+  return (
+    <form
+      className="my-4"
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit(formValues);
+      }}
+    >
+      {inputs.map(({ name, label, type, options, inputs, required }, i) => (
+        <Fragment key={i}>
+          <label className="form-label text-start w-100 mt-4">
+            {label}
+            {required ? <span className="ms-1 text-danger"> *</span> : ""}
+          </label>
+
+          {type === "select" ? (
+            <select
+              name={name}
+              className="form-control"
+              value={formValues[name]}
+              onChange={(e) =>
+                setFormValues((current) => ({
+                  ...current,
+                  [name]: e.target.value,
+                }))
+              }
+              required={required}
+            >
+              <option>Please Choose...</option>
+
+              {options?.map((option, x) => (
+                <option value={option} key={x}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          ) : type === "dynamicList" ? (
+            <table className="table table-responsive">
+              <thead>
+                <tr>
+                  {inputs?.map((input, x) => (
+                    <th key={x}>
+                      {input.label}
+                      {input.required ? (
+                        <span className="ms-1 text-danger"> *</span>
+                      ) : (
+                        ""
+                      )}
+                    </th>
+                  ))}
+
+                  <th
+                    role="button"
+                    onClick={() =>
+                      setFormValues((current) => ({
+                        ...current,
+                        [name]: formValues[name]
+                          ? [...formValues[name], {}]
+                          : [{}],
+                      }))
+                    }
+                    className="bg-success text-white"
+                  >
+                    Add
+                  </th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {formValues[name]?.map((value: dynamicObject, x = 0) => (
+                  <tr key={x}>
+                    {inputs?.map((input, y) => (
+                      <td key={y}>
+                        <input
+                          name={input.name}
+                          value={value[input.name]}
+                          onChange={(e) =>
+                            setFormValues((current) => ({
+                              ...current,
+                              [name]: current[name].map((ele = {}, z = 0) =>
+                                z === x
+                                  ? {
+                                      ...ele,
+                                      [input.name]: e.target.value,
+                                    }
+                                  : ele
+                              ),
+                            }))
+                          }
+                          required={input.required}
+                          className="form-control"
+                        />
+                      </td>
+                    ))}
+
+                    <td
+                      role="button"
+                      onClick={() =>
+                        setFormValues((current) => ({
+                          ...current,
+                          [name]: current[name].filter(
+                            (_ = {}, z = 0) => z !== x
+                          ),
+                        }))
+                      }
+                      className="bg-danger text-white"
+                    >
+                      Delete
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <input
+              name={name}
+              type={type}
+              value={formValues[name]}
+              onChange={(e) =>
+                setFormValues((current) => ({
+                  ...current,
+                  [name]: e.target.value,
+                }))
+              }
+              className="form-control"
+              required={required}
+            />
+          )}
+        </Fragment>
+      ))}
+
+      <button className="btn btn-light float-end mb-4" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
--- src/Views/Schedule/index.tsx
+++ src/Views/Schedule/index.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import * as scheduleAPI from "../../API/schedule";
 import MealView, { MealViewProps } from "../../Components/MealView";
 import PageSection from "../../Components/PageSection";
+import Form from "../../Components/Form";
 
 const Schedule = () => {
   const [data, setData] = useState<MealViewProps[][]>([]);
@@ -12,38 +13,78 @@ const Schedule = () => {
     setData(scheduleAPI.getAll());
   }, []);
 
+  const formInputs = [
+    {
+      name: "meal",
+      label: "Meal of Day",
+      type: "select",
+      options: meals,
+      required: true,
+    },
+    {
+      name: "contents",
+      label: "Meal Contents",
+      type: "dynamicList",
+      inputs: [
+        { name: "element", label: "Element", required: true },
+        { name: "count", label: "Count", required: true },
+        {
+          name: "alternatives",
+          label: "Alternatives",
+          type: "dynamicList",
+          inputs: [
+            { name: "element", label: "Element", required: true },
+            { name: "count", label: "Count", required: true },
+          ],
+          required: false,
+        },
+      ],
+      required: true,
+    },
+  ];
+
+  const onSubmit = (values = {}) => {
+    console.log("Submitting", values);
+  };
+
   return (
     <PageSection title="Scheduled Meals">
-      <table className="table table-responsive table-striped">
-        <thead>
-          <tr className="align-middle">
-            <th>Meal of Day</th>
+      <Fragment>
+        <Form inputs={formInputs} onSubmit={onSubmit} />
 
-            <th>Meal Contents</th>
-          </tr>
-        </thead>
+        <table className="table table-responsive table-striped">
+          <thead>
+            <tr className="align-middle">
+              <th>Meal of Day</th>
 
-        <tbody>
-          {data.map((mealContents, x) => (
-            <tr key={x}>
-              <th>{meals[x % meals.length]}</th>
-
-              <td>
-                <ul className="text-start">
-                  {mealContents?.map(({ element, count, alternatives }, y) => (
-                    <MealView
-                      count={count}
-                      element={element}
-                      alternatives={alternatives}
-                      key={y}
-                    />
-                  ))}
-                </ul>
-              </td>
+              <th>Meal Contents</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+
+          <tbody>
+            {data.map((mealContents, x) => (
+              <tr key={x}>
+                <th>{meals[x % meals.length]}</th>
+
+                <td>
+                  <ul className="text-start">
+                    {mealContents?.map(
+                      ({ element, count, alternatives }, y) => (
+                        <MealView
+                          count={count}
+                          element={element}
+                          alternatives={alternatives}
+                          key={y}
+                        />
+                      )
+                    )}
+                  </ul>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Fragment>
     </PageSection>
   );
 };
