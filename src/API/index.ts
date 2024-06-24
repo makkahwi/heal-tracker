a39d1288440aa5b6940ef3f3bb79b43853254141
Makkahwi <suhaibahmadai@hotmail.com>
2024-06-24 19:51:12 +0300

    Add Axios

--- src/API/index.ts
+++ src/API/index.ts
@@ -1,3 +1,13 @@
-const service = {};
+import axios from "axios";
+
+const service = axios.create({
+  baseURL:
+    "https://personal-diet-tracker-default-rtdb.europe-west1.firebasedatabase.app//",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const demoStatus = () => false;
 
 export default service;
