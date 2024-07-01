2766af2e8eb391e5df373c6b3965334ac0acec79
makkahwi <suhaibahmadai@hotmail.com>
2024-07-01 17:47:39 +0300

    Brand The App

---
+++ src/Components/Layout/Footer.tsx
@@ -0,0 +1,102 @@
+const Footer = () => {
+  const contacts = [
+    {
+      icon: "fas fa-globe",
+      link: "https://suhaib.dev/",
+      tooltip: "Own Website",
+      key: "website",
+    },
+    {
+      icon: "fab fa-linkedin",
+      link: "https://linkedin.com/in/makkahwi/",
+      tooltip: "Makkahwi @ Linkedin",
+      key: "linkedin",
+    },
+    {
+      icon: "fab fa-facebook",
+      link: "https://facebook.com/makkahwi",
+      tooltip: "Makkahwi @ Facebook",
+      key: "facebook",
+    },
+    {
+      icon: "fab fa-instagram",
+      link: "https://instagram.com/makkahwi",
+      tooltip: "Makkahwi @ Instagram",
+      key: "instagram",
+    },
+    {
+      icon: "fab fa-whatsapp",
+      link: "https://wasap.my/962788424973",
+      tooltip: "+962788424973 @ Whatsapp",
+      key: "whatsapp",
+    },
+    {
+      icon: "fab fa-telegram",
+      link: "https://t.me/makkahwi",
+      tooltip: "+962788424973 @ Telegram",
+      key: "telegram",
+    },
+    {
+      icon: "fas fa-envelope",
+      link: "mailto:SuhaibAhmadAi@hotmail.com",
+      tooltip: "Professional Email",
+      key: "email",
+    },
+    {
+      icon: "fas fa-phone",
+      link: "tel:+962788424973",
+      tooltip: "+962788424973",
+      key: "phone",
+    },
+  ];
+
+  return (
+    <footer className="footer">
+      <div className="bg-light py-4 py-md-5 py-xl-8 border-top border-light-subtle">
+        <div className="container overflow-hidden">
+          <div className="row gy-4 gy-md-0 align-items-md-center">
+            <div className="col-xs-12 col-md-7 order-1 order-md-0">
+              <h6 className="copyright text-center text-md-start">
+                PDT | Personal Diet Tracker
+              </h6>
+            </div>
+
+            <div className="col-xs-12 col-md-7 order-1 order-md-0">
+              <div className="copyright text-center text-md-start">
+                All Rights Reserved for{" "}
+                <a
+                  href="https://suhaib.dev"
+                  target="_blank"
+                  className="text-decoration-none text-dark"
+                >
+                  Suhaib Ahmad
+                </a>{" "}
+                &copy; {new Date().getFullYear()}
+              </div>
+            </div>
+
+            <div className="col-xs-12 col-md-5 order-0 order-md-1">
+              <div className="social-media-wrapper">
+                <ul className="list-unstyled m-0 p-0 d-flex justify-content-center justify-content-md-end">
+                  {contacts.map(({ tooltip, icon, link }, x) => (
+                    <li className="me-3" key={x}>
+                      <a
+                        href={link}
+                        className="link-dark link-opacity-75-hover"
+                        target="_blank"
+                      >
+                        <i className={icon} />
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
