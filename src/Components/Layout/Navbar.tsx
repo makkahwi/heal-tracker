0e689e8dc0415033b0f2592963075f29a4d815c6
makkahwi <suhaibahmadai@hotmail.com>
2024-07-01 17:20:58 +0300

    Update File Name

--- src/Components/Layout/Navbar.tsx
+++ src/Components/Layout/Navbar.tsx
@@ -1,22 +1,23 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { routes } from "../../App";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark position-fixed fixed-top w-100 m-0">
-      <div className="container-fluid">
+      <div className="container-fluid px-5 py-2">
         <span
           className="navbar-brand"
           role="button"
           onClick={() => navigate("/")}
         >
-          Personal Diet Tracker
+          PDT
         </span>
 
         <div>
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul className="nav me-auto mb-2 mb-lg-0">
             {routes.map(({ name, path }, x) => (
               <li
                 className="nav-item"
@@ -24,7 +25,16 @@ const Navbar = () => {
                 onClick={() => navigate(path)}
                 key={x}
               >
-                <span className="nav-link active">{name}</span>
+                <span
+                  className={
+                    "nav-link text-decoration-none" +
+                    (location.pathname === "/" + path
+                      ? " text-secondary"
+                      : " text-white")
+                  }
+                >
+                  {name}
+                </span>
               </li>
             ))}
           </ul>
