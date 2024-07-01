0bb4f4bccc0e3f0c7c7eb2887a16d90cb73e68a0
makkahwi <suhaibahmadai@hotmail.com>
2024-07-01 16:57:28 +0300

    Split To Pages & Add Router

---
+++ src/Views/Welcome/index.tsx
@@ -0,0 +1,30 @@
+import { Fragment } from "react/jsx-runtime";
+import PageSection from "../../Components/PageSection";
+import { routes } from "../../App";
+import { useNavigate } from "react-router-dom";
+
+const Welcome = () => {
+  const navigate = useNavigate();
+
+  return (
+    <PageSection title="Welcome To `Personal Diet Tracker`">
+      <Fragment>
+        <h4 className="mt-5 my-2">Pick of Following...</h4>
+
+        <div className="btn-group my-4">
+          {routes.map(({ name, path }, x) => (
+            <button
+              className="btn btn-light"
+              onClick={() => navigate(path)}
+              key={x}
+            >
+              {name}
+            </button>
+          ))}
+        </div>
+      </Fragment>
+    </PageSection>
+  );
+};
+
+export default Welcome;
