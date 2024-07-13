17f435bb904ca71056f7db9d111869083f996697
makkahwi <suhaibahmadai@hotmail.com>
2024-07-13 16:59:54 +0300

    Include EdgeCase UI In PageView UI

--- src/Components/PageView/index.tsx
+++ src/Components/PageView/index.tsx
@@ -1,4 +1,9 @@
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowCircleDown,
+  faArrowCircleUp,
+  faCheckCircle,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Fragment } from "react";
 
@@ -14,6 +19,8 @@ export interface props {
 }
 
 const PageView = ({ title, data, inputs, onSubmit, onDelete }: props) => {
+  const symbol = String.fromCodePoint(8734);
+
   return (
     <PageSection title={title}>
       <Fragment>
@@ -23,8 +30,16 @@ const PageView = ({ title, data, inputs, onSubmit, onDelete }: props) => {
           <table className="table table-bordered table-responsive table-striped">
             <thead>
               <tr className="align-middle">
-                {inputs.map(({ label }, x) => (
-                  <th key={x}>{label}</th>
+                {inputs.map(({ label, lowEnd, highEnd }, x) => (
+                  <th key={x}>
+                    {lowEnd || highEnd
+                      ? (lowEnd || symbol) +
+                        " > " +
+                        label +
+                        " < " +
+                        (highEnd || symbol)
+                      : label}
+                  </th>
                 ))}
 
                 <th>Actions</th>
@@ -34,8 +49,48 @@ const PageView = ({ title, data, inputs, onSubmit, onDelete }: props) => {
             <tbody>
               {data.map(({ id, ...row }, i) => (
                 <tr className="align-middle" key={i}>
-                  {inputs.map(({ name, render }, x) => (
-                    <td key={x}>{render ? render(row) : (row as any)[name]}</td>
+                  {inputs.map(({ name, render, lowEnd, highEnd }, x) => (
+                    <td
+                      className={
+                        "fw-bold " +
+                        (lowEnd && highEnd
+                          ? parseFloat((row as any)[name]) >= lowEnd &&
+                            parseFloat((row as any)[name]) <= highEnd
+                            ? "text-success"
+                            : "text-danger"
+                          : lowEnd
+                          ? parseFloat((row as any)[name]) >= lowEnd
+                            ? "text-success"
+                            : "text-danger"
+                          : highEnd
+                          ? parseFloat((row as any)[name]) <= highEnd
+                            ? "text-success"
+                            : "text-danger"
+                          : "")
+                      }
+                      key={x}
+                    >
+                      {render ? render(row) : (row as any)[name]}
+
+                      {(lowEnd || highEnd) && (row as any)[name] ? (
+                        <FontAwesomeIcon
+                          className="ms-1"
+                          icon={
+                            lowEnd
+                              ? parseFloat((row as any)[name]) >= lowEnd
+                                ? faCheckCircle
+                                : faArrowCircleDown
+                              : highEnd
+                              ? parseFloat((row as any)[name]) <= highEnd
+                                ? faCheckCircle
+                                : faArrowCircleUp
+                              : faCheckCircle
+                          }
+                        />
+                      ) : (
+                        ""
+                      )}
+                    </td>
                   ))}
 
                   <td>
@@ -63,11 +118,58 @@ const PageView = ({ title, data, inputs, onSubmit, onDelete }: props) => {
                     .filter(({ name, render }) =>
                       render ? render(row) : (row as any)[name]
                     )
-                    .map(({ label, name, render }, x) => (
+                    .map(({ label, name, render, lowEnd, highEnd }, x) => (
                       <tr className="align-middle" key={x}>
-                        <th>{label}</th>
+                        <th>
+                          {lowEnd || highEnd
+                            ? (lowEnd || symbol) +
+                              " > " +
+                              label +
+                              " < " +
+                              (highEnd || symbol)
+                            : label}
+                        </th>
 
-                        <td>{render ? render(row) : (row as any)[name]}</td>
+                        <td
+                          className={
+                            "fw-bold " +
+                            (lowEnd && highEnd
+                              ? parseFloat((row as any)[name]) >= lowEnd &&
+                                parseFloat((row as any)[name]) <= highEnd
+                                ? "text-success"
+                                : "text-danger"
+                              : lowEnd
+                              ? parseFloat((row as any)[name]) >= lowEnd
+                                ? "text-success"
+                                : "text-danger"
+                              : highEnd
+                              ? parseFloat((row as any)[name]) <= highEnd
+                                ? "text-success"
+                                : "text-danger"
+                              : "")
+                          }
+                        >
+                          {render ? render(row) : (row as any)[name]}
+
+                          {(lowEnd || highEnd) && (row as any)[name] ? (
+                            <FontAwesomeIcon
+                              className="ms-1"
+                              icon={
+                                lowEnd
+                                  ? parseFloat((row as any)[name]) >= lowEnd
+                                    ? faCheckCircle
+                                    : faArrowCircleDown
+                                  : highEnd
+                                  ? parseFloat((row as any)[name]) <= highEnd
+                                    ? faCheckCircle
+                                    : faArrowCircleUp
+                                  : faCheckCircle
+                              }
+                            />
+                          ) : (
+                            ""
+                          )}
+                        </td>
                       </tr>
                     ))}
 
