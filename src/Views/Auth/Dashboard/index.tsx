ce952c71da0eb95cb79decb0e80109851f57f17d
makkahwi <suhaibahmadai@hotmail.com>
2024-07-26 22:51:32 +0300

    Build Dashboard Comprehensive WeeklyCalendar

--- src/Views/Auth/Dashboard/index.tsx
+++ src/Views/Auth/Dashboard/index.tsx
@@ -1,13 +1,74 @@
 import { Fragment } from "react/jsx-runtime";
 
+import { useEffect, useState } from "react";
+import * as BeAPI from "../../../API";
 import PageSection from "../../../Components/PageView/PageSection";
+import { consumptionProps } from "../Diet/Consumption";
+import { medicineProps } from "../Medicine";
+import { walkExerciseProps } from "../Sports/WalkExercises";
+import WeeklyCalendar from "./WeeklyCalendar";
 
 const Dashboard = () => {
+  const [consumptionData, setConsumptionData] = useState<consumptionProps[]>(
+    []
+  );
+  const [walkExercisesData, setWalkExercisesData] = useState<
+    walkExerciseProps[]
+  >([]);
+  const [medicineData, setMedicineData] = useState<medicineProps[]>([]);
+
+  const getData = () => {
+    BeAPI.getAll("consumption")
+      .then((res: consumptionProps[]) =>
+        setConsumptionData(
+          res
+            ?.sort((a: any, b: any) => (a.timestamp > b.timestamp ? -1 : 1))
+            ?.map(({ contents, supposed, ...rest }) => ({
+              ...rest,
+              contents: contents?.sort((a, b) =>
+                a.element > b.element ? 1 : -1
+              ),
+              supposed: supposed?.sort((a, b) =>
+                a.element > b.element ? 1 : -1
+              ),
+            }))
+        )
+      )
+      .catch((err) => console.log({ err }));
+
+    BeAPI.getAll("sportSessions")
+      .then((res: any) =>
+        setWalkExercisesData(
+          res?.sort((a: walkExerciseProps, b: walkExerciseProps) =>
+            a.date > b.date ? -1 : 1
+          )
+        )
+      )
+      .catch((err) => console.log({ err }));
+
+    BeAPI.getAll("medicine")
+      .then((res: any) =>
+        setMedicineData(
+          res?.sort((a: medicineProps, b: medicineProps) =>
+            a.date > b.date ? -1 : 1
+          )
+        )
+      )
+      .catch((err) => console.log({ err }));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <PageSection title="Dashboard">
       <Fragment>
-        <h3 className="mt-5 my-2">Dashboard is Coming Soon</h3>
-        <h4 className="mt-5 my-2">Now You May Pick of Above Links</h4>
+        <WeeklyCalendar
+          consumptionData={consumptionData}
+          walkExercisesData={walkExercisesData}
+          medicineData={medicineData}
+        />
       </Fragment>
     </PageSection>
   );
