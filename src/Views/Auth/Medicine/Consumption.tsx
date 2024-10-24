734beb57bd0129613f80a8581dee4a855c72095e
Makkahwi <SuhaibAhmadAi@hotmail.com>
2024-10-24 18:25:39 +0300

    Build Incomplete Medicine Schedule Service

---
+++ src/Views/Auth/Medicine/Consumption.tsx
@@ -0,0 +1,120 @@
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Fragment, useEffect, useState } from "react";
+
+import * as BeAPI from "../../../API";
+import Form from "../../../Components/Form";
+import { MealViewProps } from "../../../Components/MealView";
+import MonthlyCalendar from "../../../Components/PageView/MonthlyCalendar";
+import PageSection from "../../../Components/PageView/PageSection";
+import { timeFormat } from "../../../Utils/consts";
+
+export interface medicineProps {
+  id?: string;
+  date: string;
+  time: string;
+  quantity: number;
+  medicine: string;
+}
+
+export const renderMedicineUI =
+  (onDelete?: Function) => (event: medicineProps, date: string, id: string) =>
+    (
+      <div>
+        {date ? (
+          <span className="d-block bg-dark text-white p-2 my-2">
+            @ {timeFormat(event.time)}{" "}
+            {onDelete && (
+              <FontAwesomeIcon
+                icon={faTrashCan}
+                className="mt-1 text-danger"
+                role="button"
+                onClick={() => onDelete(id)}
+              />
+            )}
+          </span>
+        ) : (
+          ""
+        )}
+        <div className="fw-bold">
+          {event.quantity} of {event.medicine}
+        </div>
+      </div>
+    );
+
+const MedicineConsumption = () => {
+  const [data, setData] = useState<medicineProps[]>([]);
+
+  const getData = () =>
+    BeAPI.getAll("medicine")
+      .then((res: any) =>
+        setData(
+          res?.sort((a: medicineProps, b: medicineProps) =>
+            a.date > b.date ? -1 : 1
+          )
+        )
+      )
+      .catch((err) => console.log({ err }));
+
+  useEffect(() => {
+    // scheduleAPI.getAll().then((res: MealViewProps[][]) => setData(res));
+    getData();
+  }, []);
+
+  const formInputs = [
+    {
+      name: "date",
+      label: "Date",
+      type: "date",
+      required: true,
+    },
+    {
+      name: "time",
+      label: "Time",
+      type: "time",
+      required: true,
+    },
+    {
+      name: "quantity",
+      label: "Quantity",
+      type: "number",
+      defaultValue: 1,
+      required: true,
+    },
+    {
+      name: "medicine",
+      label: "Medicine",
+      type: "select",
+      options: [{ value: "Vitamine D" }, { value: "Sleeping Bills" }],
+      defaultValue: "Vitamine D",
+      required: true,
+    },
+  ];
+
+  const onSubmit = (values: medicineProps) => {
+    BeAPI.create("medicine", values)
+      .then(() => {
+        getData();
+      })
+      .catch((err) => console.log({ err }));
+  };
+
+  const onDelete = (id: string) =>
+    BeAPI.remove("medicine", id)
+      .then(() => {
+        getData();
+      })
+      .catch((err) => console.log({ err }));
+
+  return (
+    <PageSection title="Medicine List">
+      <Fragment>
+        <Form inputs={formInputs} onSubmit={onSubmit} />
+
+        <MonthlyCalendar data={data} renderEvent={renderMedicineUI(onDelete)} />
+      </Fragment>
+    </PageSection>
+  );
+};
+
+export default MedicineConsumption;
