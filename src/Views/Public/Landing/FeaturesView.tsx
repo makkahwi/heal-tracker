fbaf1ce06dc9d30b3e84598196cdebe6c59c241f
makkahwi <suhaibahmadai@hotmail.com>
2024-11-14 13:29:37 +0300

    Build & Engage Landing View

---
+++ src/Views/Public/Landing/FeaturesView.tsx
@@ -0,0 +1,40 @@
+import { faMobileAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PageSection from "./PageSection";
+
+const FeaturesView = ({
+  title = "",
+  desc = "",
+  features = [
+    {
+      icon: faMobileAlt,
+      title: "",
+      desc: "",
+    },
+  ],
+}) => {
+  return (
+    <PageSection title={title} desc={desc}>
+      <div className="row justify-content-center align-items-stretch">
+        {features.map(({ icon, title, desc }, i) => (
+          <div key={i} className="col-md-6 col-lg-4 my-3 px-4 d-flex">
+            <div className="card h-100 shadow-sm border-0">
+              <div className="card-body text-center d-flex flex-column">
+                <FontAwesomeIcon
+                  icon={icon}
+                  className="text-primary mb-3"
+                  size="2x"
+                />
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{desc}</p>
+                <div className="mt-auto"></div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </PageSection>
+  );
+};
+
+export default FeaturesView;
