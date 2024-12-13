9890517f153b155e5a7bc6417e7d082f106b1960
Makkahwi <SuhaibAhmadAi@hotmail.com>
2024-12-13 19:41:47 +0300

    Upgrade Dependencies

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+import App from "./App";
+
+test("renders learn react link", async () => {
+  const { findByText } = render(<App />);
+  const linkElement = await findByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
