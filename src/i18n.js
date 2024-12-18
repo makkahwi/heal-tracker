3f2bf13df4a4039446795ed9aa8bf5ddbad833a3
Makkahwi <SuhaibAhmadAi@hotmail.com>
2024-12-18 13:27:42 +0300

    Activate Client Preferred Lang

--- src/i18n.js
+++ src/i18n.js
@@ -3,12 +3,15 @@ import { initReactI18next } from "react-i18next";
 import HttpApi from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
+const lang = localStorage.getItem("lang") || "en";
+
 i18n
   .use(HttpApi)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: "en",
+    lng: lang,
     debug: true,
     ns: ["translations"],
     defaultNS: "translations",
