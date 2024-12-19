a05254b9d564c85538652624adbb2c7f05c31d3f
Makkahwi <SuhaibAhmadAi@hotmail.com>
2024-12-19 12:50:57 +0300

    Add Spacing & Underline For Landing Sections

--- src/Views/Public/Landing/PageSection.tsx
+++ src/Views/Public/Landing/PageSection.tsx
@@ -1,7 +1,9 @@
 const PageSection = ({ title = "", desc = "", children = <></> }) => {
   return (
-    <section className="text-justify my-5 py-5">
-      <h2 className="mb-5">{title}</h2>
+    <section className="text-justify my-5 py-5 w-100 px-0 mx-0">
+      <h2 className="">{title}</h2>
+
+      <hr className="border border-primary bg-primary py-1 mb-5" />
 
       <h5 className="text-muted my-4">{desc}</h5>
 
