4f673ac936b74fa42eea7f373f9044e4c48e0d5c
Makkahwi <SuhaibAhmadAi@hotmail.com>
2024-12-19 13:47:38 +0300

    Add Paragraph Texts Indentation

--- src/Views/Public/Landing/PageSection.tsx
+++ src/Views/Public/Landing/PageSection.tsx
@@ -1,11 +1,11 @@
 const PageSection = ({ title = "", desc = "", children = <></> }) => {
   return (
-    <section className="text-justify my-5 py-5 w-100 px-0 mx-0">
-      <h2 className="text-primary fw-bold">{title}</h2>
+    <section className="my-5 py-5 w-100 px-0 mx-0">
+      <h2 className="text-start text-primary fw-bold">{title}</h2>
 
       <div className="bg-primary py-1 mt-3 mb-5 w-100" />
 
-      <h5 className="text-muted my-4 lh-lg">{desc}</h5>
+      <h5 className="text-justify text-muted my-4 lh-lg">{desc}</h5>
 
       <div className="my-4">{children}</div>
     </section>
