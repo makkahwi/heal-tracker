6c4ff992a1bb6228dbddd47c2e0ab1b8a1e6ebab
Makkahwi <SuhaibAhmadAi@hotmail.com>
2024-12-30 14:09:01 +0300

    Add Fasting Track Service

---
+++ src/Views/Auth/Diet/Fasting/index.tsx
@@ -0,0 +1,120 @@
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import moment from "moment";
+import { Fragment, useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import * as BeAPI from "../../../../API";
+import Form from "../../../../Components/Form";
+import MonthlyCalendar from "../../../../Components/PageView/MonthlyCalendar";
+import PageSection from "../../../../Components/PageView/PageSection";
+import { dateTotTimeFormat } from "../../../../Utils/consts";
+import i18n from "../../../../i18n";
+
+export interface fastingProps {
+  id?: string;
+  day: string;
+  breakTime: string;
+  note?: string;
+}
+
+export const renderFastingUI =
+  (onDelete?: Function) => (event: fastingProps, date: string, id: string) =>
+    (
+      <div>
+        {date ? (
+          <span className="d-block bg-dark text-white p-2 my-2">
+            {i18n.t("Services.Diet.Fasting.FastBreakingTime")} @{" "}
+            {dateTotTimeFormat(event.breakTime)}{" "}
+            {onDelete && (
+              <FontAwesomeIcon
+                icon={faTrashCan}
+                className="mt-1 text-danger"
+                role="button"
+                onClick={() => onDelete(id)}
+              />
+            )}
+            <br />
+            {event.note}
+          </span>
+        ) : (
+          ""
+        )}
+        <small>{event.note}</small>
+      </div>
+    );
+
+const Fasting = () => {
+  const { t } = useTranslation();
+
+  const [data, setData] = useState<fastingProps[]>([]);
+
+  const getData = () =>
+    BeAPI.getAll("fasting")
+      .then((res: fastingProps[]) =>
+        setData(
+          res
+            .map(({ breakTime, ...rest }) => ({
+              ...rest,
+              breakTime,
+              date: moment(breakTime).format("yyyy-MM-DD"),
+            }))
+            ?.sort((a: fastingProps, b: fastingProps) =>
+              a.breakTime > b.breakTime ? -1 : 1
+            )
+        )
+      )
+      .catch((err) => console.log({ err }));
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const formInputs = [
+    {
+      name: "day",
+      label: t("Services.Diet.Fasting.Day"),
+      type: "date",
+      required: true,
+    },
+    {
+      name: "breakTime",
+      label: t("Services.Diet.Fasting.FastBreakingTime"),
+      type: "time",
+      required: true,
+    },
+    {
+      name: "note",
+      label: t("Services.Diet.Fasting.Notes"),
+    },
+  ];
+
+  const onSubmit = (values: fastingProps) => {
+    BeAPI.create("fasting", values)
+      .then(() => {
+        getData();
+      })
+      .catch((err) => console.log({ err }));
+  };
+
+  const onDelete = (id: string) =>
+    BeAPI.remove("fasting", id)
+      .then(() => {
+        getData();
+      })
+      .catch((err) => console.log({ err }));
+
+  return (
+    <PageSection
+      title={t("Services.Diet.Fasting.Fasting")}
+      desc={t("Services.Diet.Fasting.Desc")}
+    >
+      <Fragment>
+        <Form inputs={formInputs} onSubmit={onSubmit} />
+
+        <MonthlyCalendar data={data} renderEvent={renderFastingUI(onDelete)} />
+      </Fragment>
+    </PageSection>
+  );
+};
+
+export default Fasting;
